f028a0eebd80606ddad154836a0bd5c66097ea58
XRafe <nikita-gorin98@yandex.ru>
2019-04-06 22:46:32 +0400

    Добавьте файлы проекта.

---
+++ CourseWork/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork
+{
+    public class Car
+    {
+        public int Id { set; get; }
+        public string Firm { set; get; }
+        public string Model { set; get; }
+        public int Year { set; get; }
+    }
+}
